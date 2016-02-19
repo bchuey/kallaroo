f19f85b619cdc8937a6b3acef7d2b09d6a65fbc1
Chris Huey <bchuey415@gmail.com>
2016-02-19 10:57:24 -0800

    secret key as env variable

--- kallaroo/kallaroo/kallaroo/settings.py
+++ kallaroo/kallaroo/kallaroo/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tjbzoxgiildw4*t#$0*v_e53e6t&n*fjk&o(do%l4(+)n#-_wy'
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
